8e2a7ce8430d4994a33f311b343dd4b92f83806e
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-28 02:39:38 -0600

    commit tree 42769e9ff48946f869bc9a51523baf392a988660

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter( element => name.toUpperCase() === element.toUpperCase());
+}
+
+function fuzzyMatch(array, str){
+    return array.filter( element => element.startsWith(str));
+}
+
+function matchName(array, name){
+    return array.filter( element => name.toUpperCase() === element.name.toUpperCase());
+}
